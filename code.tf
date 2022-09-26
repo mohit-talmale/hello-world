91be518c50e17ea8cb4556b08f3c99ea104bba88
Mohit Talmale <mohit1talmale@gmail.com>
2022-09-26 20:48:03 +0530

    Update code.tf

--- code.tf
+++ code.tf
@@ -263,28 +263,28 @@ resource "aws_lb_listener" "external-elb" {
   }
 }
 
-resource "aws_db_instance" "default" {
-  allocated_storage      = 10
-  db_subnet_group_name   = aws_db_subnet_group.default.id
-  engine                 = "mysql"
-  engine_version         = "8.0.28"
-  instance_class         = "db.t2.micro"
-  multi_az               = true
-  name                   = "mydb"
-  username               = "admin"
-  password               = "admin"
-  skip_final_snapshot    = true
-  vpc_security_group_ids = [aws_security_group.database-sg.id]
-}
+# resource "aws_db_instance" "default" {
+#   allocated_storage      = 10
+#   db_subnet_group_name   = aws_db_subnet_group.default.id
+#   engine                 = "mysql"
+#   engine_version         = "8.0.28"
+#   instance_class         = "db.t2.micro"
+#   multi_az               = true
+#   name                   = "mydb"
+#   username               = "admin"
+#   password               = "admin"
+#   skip_final_snapshot    = true
+#   vpc_security_group_ids = [aws_security_group.database-sg.id]
+# }
 
-resource "aws_db_subnet_group" "default" {
-  name       = "main"
-  subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-2.id]
+# resource "aws_db_subnet_group" "default" {
+#   name       = "main"
+#   subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-2.id]
 
-  tags = {
-    Name = "My DB subnet group"
-  }
-}
+#   tags = {
+#     Name = "My DB subnet group"
+#   }
+# }
 
 output "lb_dns_name" {
   description = "The DNS name of the load balancer"
