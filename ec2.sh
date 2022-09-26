0eab738d4868d9b356efec7edae948024f12f04a
Mohit Talmale <mohit1talmale@gmail.com>
2022-09-26 21:17:49 +0530

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -4,4 +4,4 @@ yum update -y
 yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
-echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+echo "<h2>EC2 running on  $(hostname -f) </h2>" > /var/www/html/index.html
