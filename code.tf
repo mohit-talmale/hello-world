f71966c41f36384743e3fa20ece9609789f389c9
Mohit Talmale <mohit1talmale@gmail.com>
2022-09-28 22:19:26 +0530

    neww

--- code.tf
+++ code.tf
@@ -242,7 +242,7 @@ resource "aws_lb" "external-elb" {
 }
 
 resource "aws_lb_target_group" "external-elb" {
-  name     = "ALB-TG"
+  name     = "ALB-TG1"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.my-vpc.id
