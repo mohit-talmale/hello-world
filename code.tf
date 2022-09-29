9ace2060433f256fc610cd0fd18fd2fb99e28b3e
Mohit Talmale <mohit1talmale@gmail.com>
2022-09-29 23:20:14 +0530

    gfg

--- code.tf
+++ code.tf
@@ -120,7 +120,7 @@ resource "aws_instance" "webserver1" {
   availability_zone      = "us-east-1a"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   subnet_id              = aws_subnet.web-subnet-1.id
-  key_name               = "developer1"
+  # key_name               = "developer1.pem"
 
   tags = {
     Name = "Web Server"
@@ -155,7 +155,7 @@ resource "aws_instance" "webserver2" {
   availability_zone      = "us-east-1b"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   subnet_id              = aws_subnet.web-subnet-2.id
-  key_name               = "developer1"
+  # key_name               = "developer1.pem"
 
   tags = {
     Name = "Web Server"
