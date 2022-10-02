d25950c892a6955dff498b5f55879eb26319e53c
Mohit Talmale <mohit1talmale@gmail.com>
2022-10-02 20:35:03 +0530

    sd

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "public_ip" {
-    value1 = try(aws_instance.webserver1.public_ip, "")
+    value = try(aws_instance.webserver1.public_ip, "")
   }
 
 output "public_ip" {
-    value2 = try(aws_instance.webserver2.public_ip, "")
+    value = try(aws_instance.webserver2.public_ip, "")
   }
