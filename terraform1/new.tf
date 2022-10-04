b0d955a2efbe0e20c7a3a6c33eb812d198fd8014
Mohit Talmale <mohit1talmale@gmail.com>
2022-10-04 12:39:55 +0530

    sf

--- terraform1/new.tf
+++ terraform1/new.tf
@@ -1,18 +1,3 @@
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
   required_providers {
     docker = {
@@ -23,7 +8,7 @@ terraform {
 }
 
 provider "docker" {
-  host = "unix:///var/run/docker.sock"
+#   host = "unix:///var/run/docker.sock"
 }
 
 resource "docker_image" "demoimage1" {
