2c685de6d1b9ee6235a8f1006048d8ff27ab36fc
Mohit Talmale <mohit1talmale@gmail.com>
2022-10-04 13:40:50 +0530

    fdsre

--- terraform1/new.tf
+++ terraform1/new.tf
@@ -1,24 +1,15 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.22.0"
-    }
+resource "docker_container" "mohit1talmale/demo-project" {
+  image = "mohit1talmale/demo-project:newtag2"
+  name  = "mohit1talmale/demo-project"
+#   restart = "always"
+#   volumes {
+#     container_path  = "/myapp"
+#     # replace the host_path with full path for your project directory starting from root directory /
+#     host_path = "/path/to/your/project/directory" 
+#     read_only = false
+#   }
+  ports {
+    internal = 8080
+    external = 80
   }
 }
-
-# provider "docker" {
-#   #   host = "unix:///var/run/docker.sock"
-# }
-
-# resource "docker_image" "demoimage1" {
-#   name = "mohit1talmale/demo-project:newtag2"
-# }
-# resource "docker_container" "foo1" {
-#   image = docker_image.demoimage1.name
-#   name  = "foo1"
-#   ports {
-#     external = 8080
-#     internal = 80
-#   }
-# }
