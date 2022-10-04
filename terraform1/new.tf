d2d2f641d4d7f55ef3083ddae8504788950e4dd6
Mohit Talmale <mohit1talmale@gmail.com>
2022-10-04 13:23:25 +0530

    ew

--- terraform1/new.tf
+++ terraform1/new.tf
@@ -1,24 +1,24 @@
-# terraform {
-#   required_providers {
-#     docker = {
-#       source  = "kreuzwerker/docker"
-#       version = "2.22.0"
-#     }
-#   }
-# }
-
-provider "docker" {
-  #   host = "unix:///var/run/docker.sock"
-}
-
-resource "docker_image" "demoimage1" {
-  name = "mohit1talmale/demo-project:newtag2"
-}
-resource "docker_container" "foo1" {
-  image = docker_image.demoimage1.name
-  name  = "foo1"
-  ports {
-    external = 8080
-    internal = 80
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.22.0"
+    }
   }
 }
+
+# provider "docker" {
+#   #   host = "unix:///var/run/docker.sock"
+# }
+
+# resource "docker_image" "demoimage1" {
+#   name = "mohit1talmale/demo-project:newtag2"
+# }
+# resource "docker_container" "foo1" {
+#   image = docker_image.demoimage1.name
+#   name  = "foo1"
+#   ports {
+#     external = 8080
+#     internal = 80
+#   }
+# }
