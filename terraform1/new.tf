0f207e89f08e88eda7c8813b6429bab0ea25f6ee
Mohit Talmale <mohit1talmale@gmail.com>
2022-10-04 12:24:52 +0530

    ds

--- terraform1/new.tf
+++ terraform1/new.tf
@@ -1,4 +1,17 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
 
 terraform {
   required_providers {
