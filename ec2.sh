43374bf78efb994d34be892c131c8e58cf7b0281
mohit-talmale <98278628+mohit-talmale@users.noreply.github.com>
2022-10-11 20:51:33 +0530

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -4,14 +4,7 @@ sudo su -
 yum update -y
 yum install docker -y
 service docker start
-yum install -y yum-utils
-yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-yum -y install terraform
-touch ~/.bashrc
-terraform -install-autocomplete
-mkdir learn-terraform-docker-container
-cd learn-terraform-docker-container
-wget https://github.com/mohit-talmale/hello-world/blob/master/terraform1/new.tf
-terraform init
-terraform apply --auto-approve
-
+docker rmi mohit1talmale/demo-project:newtag2
+docker login -u mohit1talmale -p Bunty@171
+docker pull mohit1talmale/demo-project:newtag2
+docker run -p 80:8080 mohit1talmale/demo-project:newtag2
